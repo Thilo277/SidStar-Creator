d9a25c9a0014fdc27d1a3b127a768b2834e66697
Thilo277 <thilo2707@gmail.com>
2023-02-18 11:46:38 +0100

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SidStarCreator
+{
+    public partial class MainWindow : Window
+    {
+        int i = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            String main;
+
+            main = name.Text + " " + lat.Text + " " + lon.Text;
+
+            list.Items.Add(main);
+
+            name.Text = "";
+            lat.Text = "";
+            lon.Text = "";
+
+            
+        }
+
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+            name.Text = "";
+            lat.Text = "";
+            lon.Text = "";
+            list.Items.Remove(list.SelectedItem);
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                name.Text = "";
+                lat.Text = "";
+                lon.Text = "";
+                String main;
+                main = list.SelectedItem.ToString();
+                MessageBox.Show(main);
+                String[] mains = main.Split(' ');
+                name.Text = mains[0];
+                lat.Text = mains[1];
+                lon.Text = mains[2];
+                
+            }
+            catch(NullReferenceException ex)
+            {
+                MessageBox.Show("Bitte Objekt auswählen!");
+
+            }
+            
+        }
+
+        private void edit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String main;
+
+                main = name.Text + " " + lat.Text + " " + lon.Text;
+
+                int i = list.SelectedIndex;
+
+                list.Items.Insert(i, main);
+                list.Items.RemoveAt(i + 1);
+
+
+                name.Text = "";
+                lat.Text = "";
+                lon.Text = "";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void goout_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                name.Text = "";
+                lat.Text = "";
+                lon.Text = "";
+                String splitt;
+                splitt = list.SelectedItem.ToString();
+                String[] splitted = splitt.Split(' ');
+                String na = splitted[0];
+                String la = splitted[1];
+                String lo = splitted[2];
+                String main;
+                main = $"<Sid_Waypoint ID=\"{i}\">\r\n                <Name>{na}</Name>\r\n                <Type>Normal</Type>\r\n                <Latitude>{la}</Latitude>\r\n                <Longitude>{lo}</Longitude>\r\n                <Speed>0</Speed>\r\n                <Altitude>0</Altitude>\r\n                <AltitudeCons>0</AltitudeCons>\r\n                <AltitudeRestriction>at</AltitudeRestriction>\r\n</Sid_Waypoint>";
+                output.Text += main;
+                i++;
+
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Bitte Objekt auswählen!");
+
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            output.Clear();
+            i = 0;
+        }
+    }
+}
